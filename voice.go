bf2df78a0aebc44c9983971ed9da9c92a2587bfa
jeffreymkabot <jeffrey.m.kabot@gmail.com>
2017-06-21 18:00:59 -0400

    Removed some dead code and cleanup

--- voice.go
+++ voice.go
@@ -17,9 +17,9 @@ const (
 )
 
 // Voicebox
-// voice data for a particular guild gets sent to the queue in its corresponding voicebox
+// Creating a voicebox with newVoiceBox launches a goroutine that listens to payloads on the voicebox's queue channel
+// Bot should create
 type voicebox struct {
-	guild *discordgo.Guild
 	queue chan<- *voicePayload
 	quit  chan<- struct{}
 	wait  *sync.WaitGroup
@@ -28,100 +28,6 @@ type voicebox struct {
 type voicePayload struct {
 	buffer    [][]byte
 	channelID string
-}
-
-// dispatch voice data to a particular discord guild
-// listen to a queue of voicePayloads for that guild
-// voicePayloads provide data meant for a voice channel in a discord guild
-// we can remain connected to the same channel while we process a relatively contiguous stream of voicePayloads
-// for that channel
-func (b *Bot) connectVoicebox(g *discordgo.Guild) *voicebox {
-	var vc *discordgo.VoiceConnection
-	var err error
-
-	// afk after a certain amount of time not talking
-	var afkTimer *time.Timer
-	// disconnect voice after a certain amount of time afk
-	var dcTimer *time.Timer
-
-	// disconnect() and goAfk() get invoked as the function arg in time.AfterFunc()
-	// need to use closures so they can manipulate same VoiceConnection vc used in connectVoicebox()
-	disconnect := func() {
-		if vc != nil {
-			log.Printf("Disconnect voice in guild %v %v", g.Name, g.ID)
-			_ = vc.Speaking(false)
-			_ = vc.Disconnect()
-			vc = nil
-		}
-	}
-	goAfk := func() {
-		log.Printf("Join afk channel %v in guild %v %v", g.AfkChannelID, g.Name, g.ID)
-		vc, err = b.session.ChannelVoiceJoin(g.ID, g.AfkChannelID, true, true)
-		if err != nil {
-			log.Printf("Error join afk: %#v", err)
-			disconnect()
-		} else {
-			dcTimer = time.AfterFunc(1*time.Minute, disconnect)
-		}
-	}
-	// defer goAfk()
-	queue := make(chan *voicePayload, MaxVoiceQueue)
-	quit := make(chan struct{})
-
-	go func() {
-		for {
-			select {
-			case vp := <-queue:
-				if afkTimer != nil {
-					afkTimer.Stop()
-				}
-				if dcTimer != nil {
-					dcTimer.Stop()
-				}
-				log.Printf("Speak to channel %v in guild %v %v", vp.channelID, g.Name, g.ID)
-				vc, err = b.session.ChannelVoiceJoin(g.ID, vp.channelID, false, true)
-				if err != nil {
-					log.Printf("Error join channel: %#v\n", err)
-					afkTimer = time.AfterFunc(300*time.Millisecond, goAfk)
-					break
-				}
-				_ = vc.Speaking(true)
-				// time.Sleep(100 * time.Millisecond)
-				for _, sample := range vp.buffer {
-					vc.OpusSend <- sample
-				}
-				// time.Sleep(100 * time.Millisecond)
-				_ = vc.Speaking(false)
-				afkTimer = time.AfterFunc(300*time.Millisecond, goAfk)
-			case <-quit:
-				if afkTimer != nil {
-					afkTimer.Stop()
-				}
-				if dcTimer != nil {
-					dcTimer.Stop()
-				}
-				log.Printf("Quit voice in guild %v %v", g.Name, g.ID)
-				disconnect()
-				return
-			}
-		}
-	}()
-
-	return &voicebox{
-		guild: g,
-		queue: queue,
-		quit:  quit,
-	}
-}
-
-func (b *Bot) reconnectVoicebox(g *discordgo.Guild) (err error) {
-	// TODO synchronize connect with end of quit
-	vb, ok := b.voiceboxes[g.ID]
-	if ok {
-		vb.quit <- struct{}{}
-	}
-	b.voiceboxes[g.ID] = b.connectVoicebox(g)
-	return
 }
 
 // SpeakTo opens the conversation with a discord guild
@@ -138,7 +44,7 @@ func (b *Bot) SpeakTo(g *discordgo.Guild) {
 
 func newVoiceBox(s *discordgo.Session, g *discordgo.Guild) *voicebox {
 	queue := make(chan *voicePayload, MaxVoiceQueue)
-	// close quit chammel and all go routines that receive on it will receive without blocking
+	// close quit channel and all go routines that receive on it will receive it without blocking
 	quit := make(chan struct{})
 	wg := &sync.WaitGroup{}
 	wg.Add(1)
@@ -147,6 +53,15 @@ func newVoiceBox(s *discordgo.Session, g *discordgo.Guild) *voicebox {
 		queue: queue,
 		quit:  quit,
 		wait:  wg,
+	}
+}
+
+func (vb *voicebox) close() {
+	if vb.quit != nil {
+		close(vb.quit)
+		vb.quit = nil
+		// Wait for the voicebox's payloadSender goroutine to return
+		vb.wait.Wait()
 	}
 }
 
