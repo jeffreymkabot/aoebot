6424f1b29bcd6c8b0a2cabe16a5429467d6032bc
jeffreymkabot <jeffrey.m.kabot@gmail.com>
2017-09-06 15:52:03 -0400

    Remove unused code

--- util.go
+++ util.go
@@ -1,12 +1,5 @@
 package aoebot
 
-import (
-	"math"
-	"math/rand"
-	"reflect"
-	"time"
-)
-
 var toAesthetic = map[rune]rune{
 	' ':  '　',
 	'`':  '`',
@@ -200,44 +193,4 @@ var fromAesthetic = map[rune]rune{
 	'<': '<',
 	'>': '>',
 	'？': '?',
-}
-
-// http://stackoverflow.com/questions/10485743/contains-method-for-a-slice
-func containsAny(s interface{}, t ...interface{}) bool {
-	sSlice := reflect.ValueOf(s)
-	if sSlice.Kind() == reflect.Slice {
-		set := make(map[interface{}]struct{}, sSlice.Len())
-		for i := 0; i < sSlice.Len(); i++ {
-			set[sSlice.Index(i).Interface()] = struct{}{}
-		}
-		for _, v := range t {
-			if _, ok := set[v]; ok {
-				return true
-			}
-		}
-	}
-	return false
-}
-
-func containsKeyword(s []string, t ...string) bool {
-	set := make(map[string]struct{})
-	for _, v := range s {
-		set[v] = struct{}{}
-	}
-	// fmt.Printf("set: %#v\n", set)
-	// fmt.Printf("targets: %#v\n", t)
-	for _, v := range t {
-		if _, ok := set[v]; ok {
-			// fmt.Printf("found target\n")
-			return true
-		}
-	}
-	return false
-}
-
-// TODO this doesn't work on 32-bit OS
-func randomNormalWait(m float64, s float64) time.Duration {
-	r := s*rand.NormFloat64() + m
-	r = math.Max(r, 300)
-	return time.Duration(int(r) * int(time.Second))
 }
