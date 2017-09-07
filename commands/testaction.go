9bb304667869fbd170b58ee22b2db32177a0f330
jeffreymkabot <jeffrey.m.kabot@gmail.com>
2017-09-07 17:06:37 -0400

    Consolidate test* commands into one command

--- commands/testaction.go
+++ commands/testaction.go
@@ -5,86 +5,43 @@ import (
 	"strings"
 )
 
-type TestReact struct{}
+type TestAction struct{}
 
-func (tr *TestReact) Name() string {
-	return strings.Fields(tr.Usage())[0]
+func (t *TestAction) Name() string {
+	return strings.Fields(t.Usage())[0]
 }
 
-func (tr *TestReact) Usage() string {
-	return `testreact`
+func (t *TestAction) Usage() string {
+	return `testaction`
 }
 
-func (tr *TestReact) Short() string {
-	return `Test that react actions can be dispatched`
+func (t *TestAction) Short() string {
+	return `Test that actions can be dispatched`
 }
 
-func (tr *TestReact) Long() string {
-	return tr.Short()
+func (t *TestAction) Long() string {
+	return t.Short() + "."
 }
 
-func (tr *TestReact) IsOwnerOnly() bool {
+func (t *TestAction) IsOwnerOnly() bool {
 	return false
 }
 
-func (tr *TestReact) Run(env *aoebot.Environment, args []string) error {
-	return (&aoebot.ReactAction{
+func (t *TestAction) Run(env *aoebot.Environment, args []string) error {
+	var err error
+	err = (&aoebot.ReactAction{
 		Emoji: `🤖`,
 	}).Perform(env)
-}
-
-type TestWrite struct{}
-
-func (tw *TestWrite) Name() string {
-	return strings.Fields(tw.Usage())[0]
-}
-
-func (tw *TestWrite) Usage() string {
-	return `testwrite`
-}
-
-func (tw *TestWrite) Short() string {
-	return `Test that write actions can be dispatched`
-}
-
-func (tw *TestWrite) Long() string {
-	return tw.Short()
-}
-
-func (tw *TestWrite) IsOwnerOnly() bool {
-	return false
-}
-
-func (tw *TestWrite) Run(env *aoebot.Environment, args []string) error {
-	return (&aoebot.WriteAction{
+	if err != nil {
+		return err
+	}
+	err = (&aoebot.WriteAction{
 		Content: `Hello World`,
 		TTS:     false,
 	}).Perform(env)
-}
-
-type TestVoice struct{}
-
-func (tv *TestVoice) Name() string {
-	return strings.Fields(tv.Usage())[0]
-}
-
-func (tv *TestVoice) Usage() string {
-	return `testvoice`
-}
-
-func (tv *TestVoice) Short() string {
-	return `Test that voice actions can be dispatched`
-}
-
-func (tv *TestVoice) Long() string {
-	return tv.Short()
-}
-
-func (tv *TestVoice) IsOwnerOnly() bool {
-	return false
-}
-
-func (tv *TestVoice) Run(env *aoebot.Environment, args []string) error {
+	if err != nil {
+		return err
+	}
 	return (&aoebot.VoiceAction{
 		File: `media/audio/40 enemy.dca`,
 	}).Perform(env)
