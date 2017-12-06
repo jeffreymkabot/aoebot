07bb66959f489a4fa462ebc9d459608f2e2376bc
Jeffrey Kabot <jeffrey.m.kabot@gmail.com>
2017-12-05 22:40:38 -0500

    getmemes cmd can send multiple message when there are too many conditions

--- commands/getmemes.go
+++ commands/getmemes.go
@@ -2,13 +2,16 @@ package commands
 
 import (
 	"errors"
-	"fmt"
 	"strconv"
 	"strings"
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/jeffreymkabot/aoebot"
 )
+
+// TODO worth putting in config file?
+const memesPerPage = 100
+const memesPerRow = 5
 
 type GetMemes struct{}
 
@@ -36,29 +39,57 @@ func (g *GetMemes) Run(env *aoebot.Environment, args []string) error {
 	if env.Guild == nil {
 		return errors.New("No guild")
 	}
+
 	conds := env.Bot.Driver.ConditionsGuild(env.Guild.ID)
 	if len(conds) == 0 {
 		return errors.New("no memes")
 	}
-	embed := memesEmbed(conds)
-	_, err := env.Bot.Session.ChannelMessageSendEmbed(env.TextChannel.ID, embed)
-	return err
+
+	embeds := memesEmbeds(conds)
+
+	for _, embed := range embeds {
+		_, err := env.Bot.Session.ChannelMessageSendEmbed(env.TextChannel.ID, embed)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
-func memesEmbed(conds []aoebot.Condition) *discordgo.MessageEmbed {
+func memesEmbeds(conds []aoebot.Condition) (embeds []*discordgo.MessageEmbed) {
+	title := strconv.Itoa(len(conds)) + " memes :)"
+
+	for i := 0; i < len(conds); i += memesPerPage {
+		end := i + memesPerPage
+		if end > len(conds) {
+			end = len(conds)
+		}
+
+		page := conds[i:end]
+		desc := strconv.Itoa(i) + " : " + strconv.Itoa(end)
+
+		embeds = append(embeds, memesEmbed(page, title, desc))
+	}
+	return
+}
+
+func memesEmbed(conds []aoebot.Condition, title string, desc string) *discordgo.MessageEmbed {
 	embed := &discordgo.MessageEmbed{}
-	embed.Title = fmt.Sprintf("%d memes", len(conds))
+	embed.Title = title
+	embed.Description = desc
 	embed.Fields = []*discordgo.MessageEmbedField{}
 
-	var page []aoebot.Condition
-	for i := 0; i < len(conds); i += 5 {
-		page = conds[i : i+5]
-		if i + 5 > len(conds) {
-			page = conds[i : len(conds)]
+	for i := 0; i < len(conds); i += memesPerRow {
+		end := i + memesPerRow
+		if end > len(conds) {
+			end = len(conds)
 		}
+		row := conds[i:end]
+
 		field := &discordgo.MessageEmbedField{Name: strconv.Itoa(i)}
-		for _, c := range page {
-			field.Value += c.GeneratedName() + "\n"
+		for _, cond := range row {
+			field.Value += cond.GeneratedName() + "\n"
 		}
 		embed.Fields = append(embed.Fields, field)
 	}
